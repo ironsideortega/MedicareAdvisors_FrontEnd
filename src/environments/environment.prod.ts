3f7244979eded85cb669c3297d9c5b7fface3826
Eliu Ortega <eliu.orteg@gmail.com>
2024-06-24 12:13:31 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://localhost:1434/api/',
+  backendUrl: 'http://datavista.duckdns.org:1434/',
   providerUrl: 'https://npiregistry.cms.hhs.gov/api/?limit=5&version=2.1&last_name=',
 
 };
